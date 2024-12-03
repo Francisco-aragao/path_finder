4c50bcb61e1a82960a516d45e3f810fb4cf5d471
Francisco-aragao <franciscoaragao785@gmail.com>
2024-12-03 09:46:01 -0300

    feat: more tests - not working

--- game.py
+++ game.py
@@ -7,12 +7,111 @@ TERRAIN_COSTS = {
     '@': -1 
 }
 
+import time
+
 class Game:
 
     def __init__(self, width, height):
 
-        self.width = width
-        self.height = height
-        self.map = [['' for x in range(width)] for y in range(height)]
+        self.row = width
+        self.col = height
+        self.map = [['' for _ in range(width)] for _ in range(height)]
+
+
+    def store_map(self, map_file):
+
+        with open(map_file, encoding='UTF-8') as f:
+            
+            lines = f.readlines()[1:]  # Skip the first line with the dimensions
     
+            # Populate the board with coordinates (X, Y)
+            for x, line in enumerate(lines):  # `y` represents the row
+                for y, char in enumerate(line.strip()):  # `x` represents the column
+                    self.map[y][x] = char
+        
+         # print map
+        for row in self.map:
+            print(row)
+        
+
+    def dfs(self, initial_x, initial_y, goal_x, goal_y):
+        
+        # It is not optimal
+
+        stack = [(initial_x, initial_y, 0, [])]
+        visited = set()
+        min_cost = float('inf')
+        min_path = []
+
+        while stack:
+
+            x, y, cost, path = stack.pop()
+
+            if (x, y) in visited or x >= self.col or y >= self.row or x < 0 or y < 0:
+                continue
+
+            print('Visiting', x, y , ':', self.map[x][y])
+
+            visited.add((x, y))
+
+            path.append((x, y))
+
+            if (x, y) == (goal_x, goal_y):
+                if cost < min_cost:
+                    min_cost = cost
+                    min_path = path
+                continue
+
+            cost += TERRAIN_COSTS[self.map[x][y]]
+
+            directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+            for dx, dy in directions:
+                stack.append((x + dx, y + dy, cost, path.copy()))
+
+        print(min_path)
+        print(min_cost)
+        return min_path, min_cost
     
+
+    def bfs(self, initial_x, initial_y, goal_x, goal_y):
+        
+        # Implement the Breadth First Search algorithm to find the shortest path
+
+        queue = [(initial_x, initial_y, 0, [])]
+        visited = set()
+        min_cost = float('inf')
+        min_path = []
+
+        while queue:
+
+            x, y, cost, path = queue.pop(0)
+
+            
+            if (x, y) in visited or x >= self.col or y >= self.row or x < 0 or y < 0:
+                continue
+
+            print('Visiting', x, y , ':', self.map[x][y])
+
+            visited.add((x, y))
+
+            path.append((x, y))
+
+            if (x, y) == (goal_x, goal_y):
+                if cost < min_cost:
+                    min_cost = cost
+                    min_path = path
+                continue
+
+            cost += TERRAIN_COSTS[self.map[x][y]]
+
+            directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+            for dx, dy in directions:
+                queue.append((x + dx, y + dy, cost, path.copy()))
+            
+            visited.remove((x, y))
+
+        print(min_path)
+        print(min_cost)
+        return min_path, min_cost
--- main.py
+++ main.py
@@ -10,7 +10,19 @@ if __name__ == '__main__':
     # index is based on COLS x ROWS
     width, height = utils.extract_map_dimensions(args.map_file)
 
-    game = Game(width, height)
+    print('Width', width, 'Height', height)
 
+    game = Game(width=height, height=width)
+
+    game.store_map(args.map_file)
+
+    # inverting the coordinates
+    """ initial_x = args.initial_y
+    initial_y = args.initial_x
+
+    goal_x = args.goal_y
+    goal_y = args.goal_x """
+
+    game.bfs(args.initial_x, args.initial_y, args.goal_x, args.goal_y)
 
 
